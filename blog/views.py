500002ab50cdd65852d3c2237e01bb1baa37e856
Fatir Yousaf <fatiryousaf@gmail.com>
2024-07-15 11:58:47 -0400

    Added html templates linked to a base template

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+posts = [
+    {
+    'author': 'CoreyMS',
+    'title': 'Blog Post 1',
+    'content': 'First post content',
+    'date_posted': 'August 27, 2018'
+},
+{
+    'author': 'Jane Doe',
+    'title': 'Blog Post 2',
+    'content': 'Second post content',
+    'date_posted': 'August 28, 2018'
+},
+{
+    'author': 'John Doe',
+    'title': 'Blog Post 3',
+    'content': 'Third post content',
+    'date_posted': 'August 30, 2018'
+}
+]
 def home(request):
-    return HttpResponse('blog/home.html')
+    context = {'posts': posts, 'title': 'Home'}
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
