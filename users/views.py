ecd343c68a63c9eec5498d03b330b2f81f80e0ce
Fatir Yousaf <fatiryousaf@gmail.com>
2024-07-22 15:43:03 -0400

    Large images will now get resized so they take up less space

--- users/views.py
+++ users/views.py
@@ -34,7 +34,7 @@ def profile(request):
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
-        
+    
     context = {
         "u_form": u_form,
         "p_form": p_form,
