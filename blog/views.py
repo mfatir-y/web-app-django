292680ec57465c48c8bff91d0714f2b5c6bc21c4
Fatir Yousaf <fatiryousaf@gmail.com>
2024-07-23 16:10:35 -0400

    Made a class based list view of posts for the home page

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from . import models
+from django.views.generic import ListView
 
 def home(request):
     context = {'posts': models.Post.objects.all(), 'title': 'Home'}
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = models.Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
