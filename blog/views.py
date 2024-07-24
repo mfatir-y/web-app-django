932d7f617d08edbad03e6fbfc1283fdfdb6c83d8
Fatir Yousaf <fatiryousaf@gmail.com>
2024-07-24 18:10:39 -0400

    Made a class based detail view for posts on home page along with a post detail template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from . import models
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 def home(request):
     context = {'posts': models.Post.objects.all(), 'title': 'Home'}
@@ -11,6 +11,9 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = models.Post
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
