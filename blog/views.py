c4504f8e19b501a23ca292724328c5f26dfc97db
Fatir Yousaf <fatiryousaf@gmail.com>
2024-07-24 18:28:13 -0400

    Made an update view for individual posts with a post form template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from . import models
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def home(request):
     context = {'posts': models.Post.objects.all(), 'title': 'Home'}
@@ -14,6 +15,14 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = models.Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = models.Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
