543d49f9832c5c7eb7b1dd42b8c99b8909872afc
Fatir Yousaf <fatiryousaf@gmail.com>
2024-08-08 17:17:48 -0400

    Added Password Reset URL and template

--- webApp_coreyschafer/urls.py
+++ webApp_coreyschafer/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path("register/", user_views.register, name="register"),
     path("login/", auth_views.LoginView.as_view(template_name='users/login.html') , name="login"),
     path("logout/", user_views.user_logout , name="logout"),
+    path("password-reset/", auth_views.PasswordResetView.as_view(template_name='users/password_reset.html') , name="password_reset"),
     path("profile/", user_views.profile , name="profile"),
 ]
 
