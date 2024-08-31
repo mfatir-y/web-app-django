10465db8a916db34cdcd019f067591ee19e3eb53
Fatir Yousaf <fatiryousaf@gmail.com>
2024-08-30 23:25:03 -0400

    Modified latest posts section on sidebar and added user signed in visuals

--- blog/views.py
+++ blog/views.py
@@ -4,30 +4,40 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 
-
-def home(request):
-    context = {'posts': models.Post.objects.all(), 'title': 'Home'}
-    return render(request, 'blog/home.html', context)
-
 class PostListView(ListView):
     model = models.Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 5
+    paginate_by = 8
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_posts'] = models.Post.objects.all().order_by('-date_posted')[:3]
+        return context
 
 class UserPostListView(ListView):
     model = models.Post
     template_name = 'blog/user_posts.html'
     context_object_name = 'posts'
-    paginate_by = 5
+    paginate_by = 10
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
         return models.Post.objects.filter(author=user).order_by('-date_posted')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_posts'] = models.Post.objects.all().order_by('-date_posted')[:3]
+        return context
 
 class PostDetailView(DetailView):
     model = models.Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_posts'] = models.Post.objects.all().order_by('-date_posted')[:3]
+        return context
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = models.Post
@@ -36,6 +46,11 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_posts'] = models.Post.objects.all().order_by('-date_posted')[:3]
+        return context
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = models.Post
@@ -58,4 +73,5 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         return self.request.user == post.author
 
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About'})
+    context = {'latest_posts': models.Post.objects.all().order_by('-date_posted')[:3], 'title': 'About'}
+    return render(request, 'blog/about.html', context)
